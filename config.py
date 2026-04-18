46c849eeac2e780ccf4ab63ba391d130f95394ff
Raphaeël Saulière <raphael_sauliere@hotmail.com>
2026-04-18 09:06:51 +0200

    fix: Comment out unused ALTO_DIRS paths for clarity in configuration

--- config.py
+++ config.py
@@ -40,12 +40,12 @@ EMBEDDING_DIM_V2 = 96
 FF_DIM_V2 = 192
 
 ALTO_DIRS = [
-    "D:/OCR_genealogie/Alto/bars_dordogne_alto",
-    "D:/OCR_genealogie/Alto/saint_chamassy_dordogne_alto_set_1",
+    # "D:/OCR_genealogie/Alto/bars_dordogne_alto",
+    # "D:/OCR_genealogie/Alto/saint_chamassy_dordogne_alto_set_1",
     ## "D:/OCR_genealogie/Alto/saint_chamassy_dordogne_alto_set_train",
-    # "D:/OCR_genealogie/Alto/lectaurep_bronod_notaire_paris_18e",
-    # "D:/OCR_genealogie/Alto/lectaurep_mariages_divorces_paris_19e",
-    # "D:/OCR_genealogie/Alto/lectaurep_repertoires_notaires_paris_1830-1939",
+    "D:/OCR_genealogie/Alto/lectaurep_bronod_notaire_paris_18e",
+    "D:/OCR_genealogie/Alto/lectaurep_mariages_divorces_paris_19e",
+    "D:/OCR_genealogie/Alto/lectaurep_repertoires_notaires_paris_1830-1939",
     # "D:/OCR_genealogie/Alto/timeuscorpus_prudhommes_paris_1858-1878",
 ]
 
